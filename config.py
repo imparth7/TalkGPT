df1f2c3b702ecf243f4b4a6f3fe37e0cfc9902ff
Parth <108121667+imparth7@users.noreply.github.com>
2023-02-16 23:03:21 +0530

    Update config.py
    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 Api = Enter your openai api key here.
 # How to get openai api key
-# Go platform.openai.com/payground website
+# Go platform.openai.com/playground website
 # right side go on profile
 # Click on View api key
 # Create new secret key
